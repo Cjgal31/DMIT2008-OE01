750580516e720f6ba53ec021b71786422b5ff07a
oa <oliver+github@abolish.golf>
2026-07-02 17:52:15 -0600

    add [02 - react intro]: ex05 - write todoItem  into todo list (updating arrays with state)

--- 02 - react intro/examples/05 - state/instructor/state-intro/components/TodoList.jsx
+++ 02 - react intro/examples/05 - state/instructor/state-intro/components/TodoList.jsx
@@ -14,6 +14,7 @@ export default function TodoList() {
   // second term: the setter function; the only thing allowed to change that value
   // initial param for useState: initial value
   const [todoItem, setTodoItem] = useState("")
+  const [allTodos, setAllTodos] = useState([])
 
   const onTodoTextChange = (event) => {
     // call state variable's setter w/ new value to write.
@@ -22,6 +23,16 @@ export default function TodoList() {
 
   const onAddTodoClick = () => {
     console.log("clicked!")
+    // we can't just e.g. push a new element to the array, because the setter function is the only thing
+    // that can change the stateful variable's value, so I need to entirely reconstruct the new array.
+    // Here, that just means... everything I had before, plus the new item.
+    const newTodos = [...allTodos, todoItem]
+    // We'll use the spread operator (...) for that. Basically, it means taking all elements of a series (e.g. from an array)
+    // without actually *being* in that container.
+    // The line above says: "take the *elements inside* allTodos, tack on todoItem after that, and wrap everything
+    // in a new array".
+    setAllTodos(newTodos)
+    console.log(allTodos)
   }
 
   return (
